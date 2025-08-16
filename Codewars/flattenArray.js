e0c7a2ec14dbb2b4ee99e809bd6e9a97c6a5ce11
Olivier-Masabo <masaboolivier21@gmail.com>
2025-08-16 22:58:43 +0200

    encrypt message

--- Codewars/flattenArray.js
+++ Codewars/flattenArray.js
@@ -1,4 +1,16 @@
- function flattenAndSort(array) {
- return array.flat().sort((a,b) => a - b)
+//  function flattenAndSort(array) {
+//  return array.flat().sort((a,b) => a - b)
+// }
+// console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]))
+
+
+// Encrypting string...
+
+var encryptThis = function(text) {
+ return text.split(" ").map(word =>{
+    if(word < 1)return false;
+return word.charCodeAt(0)+ word[word.length - 1] +  word.slice(2, -1) + word[1]
+ }).join(" ")
+
 }
-console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]))
+console.log(encryptThis("Hello world"))
