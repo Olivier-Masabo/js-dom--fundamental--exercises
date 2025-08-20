6870fa900883c37cd3edabf6b87ef117576a75e3
Olivier-Masabo <masaboolivier21@gmail.com>
2025-08-20 23:23:32 +0200

    creating simple calculator

--- Codewars/multiplication.js
+++ Codewars/multiplication.js
@@ -7,3 +7,31 @@
  }
 
  console.log(multiplied(4))
+
+ // simple calculator using javaScript
+
+ 
+function calculator(a,b,sign){
+ if( typeof a !=="number" ||  typeof b !=="number" ) return "unknown value"
+ switch(sign){
+      
+   case "+":
+   return a + b
+   break;
+    case "-":
+   return a - b
+   break;
+    case "/":
+   return a / b
+   break;
+    case "*":
+   return a * b
+   break;
+    default :
+   return "unknown value";
+   break;
+ }
+   
+
+  }
+  console.log(calculator(3,7,"-"))
