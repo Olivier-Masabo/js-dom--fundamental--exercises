8ddd6f98336a5cb5137878b890530a797ff228ec
Olivier-Masabo <masaboolivier21@gmail.com>
2025-08-20 23:46:08 +0200

    finding summation

--- Codewars/multiply.js
+++ Codewars/multiply.js
@@ -7,3 +7,15 @@
  }
 
  console.log(multiply(3,5))
+
+
+ // finding summation 
+ var summation = function (num) {
+    let result=[]
+    for (let i = 1; i <=num; i++){
+       result.push(i)
+    }
+    return result.reduce((prev,curr) => prev + curr);
+ 
+}
+summation(8)
