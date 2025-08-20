57a8e160e732d191e58d54e92fe1934dc7bc3793
Olivier-Masabo <masaboolivier21@gmail.com>
2025-08-20 23:13:26 +0200

    getting first non-repeated

--- Codewars/flattenArray.js
+++ Codewars/flattenArray.js
@@ -1,7 +1,7 @@
-//  function flattenAndSort(array) {
-//  return array.flat().sort((a,b) => a - b)
-// }
-// console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]))
+ function flattenAndSort(array) {
+ return array.flat().sort((a,b) => a - b)
+}
+console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]))
 
 
 // Encrypting string...
@@ -14,3 +14,17 @@ return word.charCodeAt(0)+ word[word.length - 1] +  word.slice(2, -1) + word[1]
 
 }
 console.log(encryptThis("Hello world"))
+
+// First non-repeated character
+
+function firstNonRepeatingLetter(s) {
+ const arr = s.split("")
+ const lowercase = s.toLowerCase()
+ for(let i=0;i<arr.length;i++){
+   if(lowercase.indexOf(lowercase[i]) === lowercase.lastIndexOf(lowercase[i])){
+     return arr[i]
+   }
+ }
+  return ""
+}
+firstNonRepeatingLetter("moonmen")
