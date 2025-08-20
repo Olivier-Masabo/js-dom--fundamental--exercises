e7826fc8c68847a25f9e9f235c805dc263c19d9b
Olivier-Masabo <masaboolivier21@gmail.com>
2025-08-20 23:15:03 +0200

    lottery maching finding number

--- Codewars/findOccurance.js
+++ Codewars/findOccurance.js
@@ -8,3 +8,17 @@
     return newArr;
  }
  console.log(findOccur([1,2,3,4,5,6,7,8,5,6,5],5))
+
+ // find lottery machine
+
+ function lottery(str){
+  let checking = /\d/ig
+  let result =str.match(checking)
+  if(result){
+    
+  let final =[...new Set(result)].join("")
+  return final
+  }else{
+    return "One more run!"
+  }
+}
