0b450872bc4ad56ee2376ae332906a873cf6e0bf
Olivier-Masabo <masaboolivier21@gmail.com>
2025-09-05 19:55:36 +0200

    two sum program

--- Codewars/findOccurance.js
+++ Codewars/findOccurance.js
@@ -22,3 +22,17 @@
     return "One more run!"
   }
 }
+
+//two sum
+
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j]; 
+      }
+    }
+  }
+}
+
+console.log(twoSum([1, 2, 3])); 
