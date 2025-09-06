c7777fcfcc69d7a832150d478d025bf84a4840e2
Olivier-Masabo <masaboolivier21@gmail.com>
2025-09-06 20:32:22 +0200

    shifting and adding letter at the end

--- Codewars/objectTrans.js
+++ Codewars/objectTrans.js
@@ -7,3 +7,14 @@
 for(let value in inputObject){
     console.log(`${value.toUpperCase()}: ${inputObject[value]}`)
 }
+
+//shifting first letter at the end and remain special character unchangable
+
+function pigIt(str) {
+  let words = str.split(" ").map(item => {
+    return /^[a-zA-Z]+$/.test(item) 
+      ? item.slice(1) + item.charAt(0) + "ay" 
+      : item;
+  });
+  return words.join(" ");
+}
